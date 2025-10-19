ac6af87a90ebb3b20b5a23cbf3aec6e5ec32f2c7
zuuzii <62288975+zuuzii@users.noreply.github.com>
2025-10-19 14:25:08 +0700

    Delete Keymap.c

--- Keymap.c
+++
@@ -1,7 +0,0 @@
-#ifdef ENCODER_MAP_ENABLE
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [0] = {
-        ENCODER_CCW_CW(KC_LBRC, KC_RBRC), // Encoder kiri: [ dan ]
-    },
-};
-#endif
