6fc5796d09c7a356cf2a4e39e5d09fb026838643
zuuzii <62288975+zuuzii@users.noreply.github.com>
2025-10-19 15:39:55 +0700

    Update keymap.c

--- keymaps/mykeymap/keymap.c
+++ keymaps/mykeymap/keymap.c
@@ -1,7 +1,8 @@
 #ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [0] = {
-        ENCODER_CCW_CW(KC_LBRC, KC_RBRC), // Encoder kiri: [ dan ]
-    },
+    [0] = { ENCODER_CCW_CW(KC_LBRC, KC_RBRC) },  // Layer 0 → [ dan ]
+    [1] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },  // Layer 1 → tidak berubah
+    [2] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },  // Layer 2 → tidak berubah
+    [3] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },  // Layer 3 → tidak berubah
 };
 #endif
