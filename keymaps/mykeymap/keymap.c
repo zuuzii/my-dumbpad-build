9dba66442cf4d08da5b226c01dc9113d3e1d9bea
zuuzii <62288975+zuuzii@users.noreply.github.com>
2025-10-19 16:46:21 +0700

    Update keymap.c

--- keymaps/mykeymap/keymap.c
+++ keymaps/mykeymap/keymap.c
@@ -1,6 +1,6 @@
 #ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [0] = { ENCODER_CCW_CW(KC_LBRC, KC_RBRC) },  // Layer 0 → [ dan ]
+    [0] = { ENCODER_CCW_CW(LCTL(KC_MINUS), LCTL(KC_EQUAL)) },  // Ctrl + - dan Ctrl + +
     [1] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },  // Layer 1 → tidak berubah
     [2] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },  // Layer 2 → tidak berubah
     [3] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },  // Layer 3 → tidak berubah
