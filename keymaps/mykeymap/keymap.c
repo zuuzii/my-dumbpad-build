754e30f0bb4b67b7ad2aa40608162b6b8a658223
zuuzii <62288975+zuuzii@users.noreply.github.com>
2025-10-19 17:52:12 +0700

    Update keymap.c

--- keymaps/mykeymap/keymap.c
+++ keymaps/mykeymap/keymap.c
@@ -6,3 +6,9 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [3] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },  // Layer 3 → tidak berubah
 };
 #endif
+bool encoder_button_pressed(uint8_t index, bool pressed) {
+    if (pressed && index == 0) {  // encoder pertama ditekan
+        tap_code16(LCTL(KC_S));   // Ctrl + S
+    }
+    return false;
+}
